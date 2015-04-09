fc86d0af457eac0d90dd7c5e6604f1574e6c7256
Christoffer Winterkvist <chris@zenangst.com>
2015-04-09 14:20:28 +0200

    Setup AppDelegate

--- Pod/Demo/AppDelegate.swift
+++ Pod/Demo/AppDelegate.swift
@@ -2,13 +2,18 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
+
     var window: UIWindow?
-    
+
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        
-        let viewController: TutorialViewController
-        
+
+        let viewController = TutorialViewController.new()
+        let navigationController = UINavigationController(rootViewController: viewController)
+
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
+
         return true
     }
 }
