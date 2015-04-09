e656f6f6ee4656f6d4cfde9ceb5c74820886b759
Christoffer Winterkvist <chris@zenangst.com>
2015-04-09 15:02:20 +0200

    Add demo views

--- Pod/Demo/AppDelegate.swift
+++ Pod/Demo/AppDelegate.swift
@@ -8,6 +8,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
         let viewController = TutorialViewController(title: "Tutorial")
+        let bounds = UIScreen.mainScreen().bounds;
+        let view1 = UIView(frame: bounds)
+        view1.backgroundColor = UIColor.greenColor()
+        let view2 = UIView(frame: bounds)
+        view2.backgroundColor = UIColor.yellowColor()
+        viewController.addPage(view1)
+        viewController.addPage(view2)
+
         let navigationController = UINavigationController(rootViewController: viewController)
 
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
