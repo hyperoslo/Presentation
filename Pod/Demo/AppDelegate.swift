c6dc6ecfe5c08b1e3ffe14938b61409f14b120cc
Elvis Nuñez <elvisnunez@me.com>
2015-04-09 14:07:30 +0200

    Simplify project structure

--- Pod/Demo/AppDelegate.swift
+++ Pod/Demo/AppDelegate.swift
@@ -1,15 +1,14 @@
 import UIKit
-import Tutorial
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-
+        
         let viewController: TutorialViewController
-
+        
         return true
     }
 }
