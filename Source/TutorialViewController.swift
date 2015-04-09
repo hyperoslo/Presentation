8e48ac4d80e456cce38c79303458f7b7155aaecb
Christoffer Winterkvist <chris@zenangst.com>
2015-04-09 19:46:52 +0200

    Implement live bounds

--- Source/TutorialViewController.swift
+++ Source/TutorialViewController.swift
@@ -2,16 +2,16 @@ import UIKit
 
 let TutorialPageControlHeight: CGFloat = 37.0
 
-
 public class TutorialViewController : UIViewController,UIScrollViewDelegate {
 
     let TutorialDeviceRotatedNotification = "deviceDidRotate"
 
     private var scrollView: UIScrollView = {
-        let bounds = UIScreen.mainScreen().bounds
+        let bounds = UIScreen.mainScreen().liveBounds()
         let frame = CGRectMake(0.0, 0.0, bounds.size.width, bounds.size.height)
         let scrollView = UIScrollView(frame: bounds)
 
+        scrollView.backgroundColor = UIColor.redColor()
         scrollView.pagingEnabled = true
         scrollView.showsHorizontalScrollIndicator = false
         scrollView.autoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight
@@ -20,7 +20,7 @@ public class TutorialViewController : UIViewController,UIScrollViewDelegate {
     }()
 
     private var pageControl: UIPageControl  = {
-        let bounds = UIScreen.mainScreen().bounds
+        let bounds = UIScreen.mainScreen().liveBounds()
         let frame = CGRectMake(bounds.size.width / 4,
             bounds.size.height / 1.5,
             bounds.size.width / 2,
@@ -33,6 +33,10 @@ public class TutorialViewController : UIViewController,UIScrollViewDelegate {
 
         return pageControl
     }()
+
+    public var currentPage: Int = 0 {
+        didSet { self.scrollToPageWithIndex(currentPage) }
+    }
 
     convenience init(title :String) {
         self.init()
@@ -51,6 +55,7 @@ public class TutorialViewController : UIViewController,UIScrollViewDelegate {
         self.scrollView.delegate = self
 
         self.view.addSubview(self.scrollView)
+        self.view.addSubview(self.pageControl)
     }
 
     public override func viewWillAppear(animated: Bool) {
@@ -79,7 +84,7 @@ public class TutorialViewController : UIViewController,UIScrollViewDelegate {
     // MARK: Public methods
 
     public func addPage(view: UIView) {
-        let bounds = UIScreen.mainScreen().bounds
+        let bounds = UIScreen.mainScreen().liveBounds()
 
         self.scrollView.contentSize = CGSizeMake(
             self.scrollView.contentSize.width + self.scrollView.bounds.size.width,
@@ -102,17 +107,40 @@ public class TutorialViewController : UIViewController,UIScrollViewDelegate {
     }
 
     private func invalidateLayout() {
-        let bounds = UIScreen.mainScreen().bounds
+        let bounds = UIScreen.mainScreen().liveBounds()
         var index = 0
-        for obj in self.view.subviews {
-            var view = obj as! UIView
+        var heightOffset: CGFloat = 0.0
+
+        if let navigationBar = self.navigationController?.navigationBar {
+            heightOffset += navigationBar.frame.origin.y
+            heightOffset += navigationBar.frame.size.height
+        }
+
+        for subView in self.scrollView.subviews {
+            var view = subView as! UIView
             var frame = view.frame
+
+            if index > 0 {
+                frame.origin.x = bounds.size.width * CGFloat(index)
+            }
+
             frame.size.width = bounds.size.width
-            frame.size.height = bounds.size.height
-
+            frame.size.height = self.scrollView.frame.size.height
             view.frame = frame
-
             ++index
         }
+
+        self.scrollView.contentSize = CGSizeMake(
+            bounds.size.width * CGFloat(index - 1),
+            bounds.size.height - heightOffset
+        );
+
+        self.scrollToPageWithIndex(self.pageControl.currentPage)
+    }
+
+    private func scrollToPageWithIndex(pageIndex: Int) {
+        let bounds = UIScreen.mainScreen().liveBounds()
+        let view = self.scrollView.subviews[pageIndex] as! UIView
+        self.scrollView.scrollRectToVisible(view.frame, animated: true)
     }
 }
