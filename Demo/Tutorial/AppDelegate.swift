4354c477fd1b8c2b01a4212a96b6c5cfad3612cc
Christoffer Winterkvist <chris@zenangst.com>
2015-04-09 13:01:15 +0200

    Add Demo project

---
+++ Demo/Tutorial/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Tutorial
+//
+//  Created by Christoffer Winterkvist on 4/9/15.
+//
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
+
