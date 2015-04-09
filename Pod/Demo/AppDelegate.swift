27a5efa572bf0b956658424c2a470d337de8dbb4
Christoffer Winterkvist <chris@zenangst.com>
2015-04-09 14:40:47 +0200

    Init with title

--- Pod/Demo/AppDelegate.swift
+++ Pod/Demo/AppDelegate.swift
@@ -7,7 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
-        let viewController = TutorialViewController.new()
+        let viewController = TutorialViewController(title: "Tutorial")
         let navigationController = UINavigationController(rootViewController: viewController)
 
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
