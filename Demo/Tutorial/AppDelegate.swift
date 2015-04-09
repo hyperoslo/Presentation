c2b72dfcb414fdf4ff6e45ae057871d0d676d49b
Christoffer Winterkvist <chris@zenangst.com>
2015-04-09 13:27:13 +0200

    Remove newline

--- Demo/Tutorial/AppDelegate.swift
+++ Demo/Tutorial/AppDelegate.swift
@@ -6,7 +6,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-
         return true
     }
 }
