ee5e32eb9fdc28911e965bcc3e8f7dd2b33df7bc
Christoffer Winterkvist <chris@zenangst.com>
2015-04-15 20:49:36 +0200

    Organize extensions

--- Source/TutorialController.swift
+++ Source/TutorialController.swift
@@ -49,3 +49,51 @@ let MinimumMarginLateralSpace: CGFloat = 20.0
   }
 
 }
+
+public extension UIViewController {
+
+  convenience init(model: TutorialModel) {
+    self.init(nibName: nil, bundle: nil)
+    addModel(model)
+  }
+
+  public func addModel(model: TutorialModel) {
+    for modelView in model.views() {
+      self.view.addSubview(modelView as! UIView)
+    }
+    layoutSubviews()
+  }
+
+  func layoutSubviews() {
+    let bounds = UIScreen.mainScreen().bounds
+    var y: CGFloat = 0.0
+
+    for object in self.view.subviews {
+      var view = object as! UIView
+      var frame = bounds
+
+      if object.isKindOfClass(UIImageView.classForCoder()) {
+        let imageSize = (object as! UIImageView).image!.size
+
+        frame.size.width = imageSize.width
+        frame.size.height = imageSize.height
+        frame.origin.x = bounds.size.width / 2 - imageSize.width / 2
+        frame.origin.y = bounds.size.height / 2 - imageSize.height / 2
+        y = frame.origin.y + frame.height
+      } else {
+        frame.size.width = bounds.width - MinimumMarginLateralSpace * 2
+        frame.size.height = bounds.height / 2 / 4
+        frame.origin.x = bounds.size.width / 2 - frame.size.width / 2
+
+        if y == 0.0 {
+          y = bounds.height / 2 - frame.size.height / 2
+        }
+
+        frame.origin.y = y
+        y += frame.size.height
+      }
+
+      view.frame = frame
+    }
+  }
+}
