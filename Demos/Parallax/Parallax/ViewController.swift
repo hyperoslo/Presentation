2a647ad4018925bacc780ff0ead8ae10510141d1
Vadym Markov <impressionwave@gmail.com>
2015-05-04 00:52:53 +0200

    Create project

---
+++ Demos/Parallax/Parallax/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Parallax
+//
+//  Created by Vadym Markov on 04/05/15.
+//  Copyright (c) 2015 Hyper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
